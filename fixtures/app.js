1fdb6d549499d8d9596fa27286f4978f667bbd62
Kaique Silva <kaeyosthaeron@gmail.com>
2020-05-11 21:46:53 -0300

    Options

--- fixtures/app.js
+++ fixtures/app.js
@@ -17,6 +17,8 @@ const outcomeService = client.service('v1/outcomes');
 if (args.length === 2 && args[0] === '--income') {
   incomeService.create({
     value: parseFloat(args[1], 10),
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }).then(res => {
     console.log('Income created: ', res);
     process.exit(0);
@@ -24,9 +26,20 @@ if (args.length === 2 && args[0] === '--income') {
     console.debug(err);
     process.exit(1);
   });
+} else if (args.length === 1 && args[0] === '--incomes-total') {
+  incomeService.find({ query: {} }).then(res => {
+    let out = 0;
+    res.data.forEach(income => {
+      out += income.value;
+    });
+    console.log('Incomes total: ', out);
+    process.exit(0);
+  });
 } else if (args.length === 2 && args[0] === '--outcome') {
   outcomeService.create({
     value: parseFloat(args[1], 10),
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }).then(res => {
     console.log('Outcome created: ', res);
     process.exit(0);
@@ -34,4 +47,43 @@ if (args.length === 2 && args[0] === '--income') {
     console.debug(err);
     process.exit(1);
   });
+} else if (args.length === 1 && args[0] === '--outcomes-total') {
+  incomeService.find({ query: {} }).then(res => {
+    let out = 0;
+    res.data.forEach(outcome => {
+      out += outcome.value;
+    });
+    console.log('Outcomes total: ', out);
+    process.exit(0);
+  });
+} else if (args.length === 1 && args[0] === '--convergence') {
+  async function convergence() {
+    try {
+      const incomes = await incomeService.find({ query: {} });
+      const outcomes = await outcomeService.find({ query: {} });
+      let incomesTotal = 0;
+      let outcomesTotal = 0;
+      incomes.data.forEach(income => {
+        incomesTotal += income.value;
+      });
+      outcomes.data.forEach(outcome => {
+        outcomesTotal += outcome.value;
+      });
+      console.log('Convergence: ', incomesTotal - outcomesTotal);
+      process.exit(0);
+    } catch (e) {
+      console.debug(e);
+      process.exit(1);
+    }
+  }
+
+  convergence();
+} else {
+  console.log('Usage of finances app: ');
+  console.log('  --income [Number]');
+  console.log('  --outcome [Number]');
+  console.log('  --incomes-total');
+  console.log('  --outcomes-total');
+  console.log('  --convergence');
+  process.exit(0);
 }
